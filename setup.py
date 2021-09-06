e0f961f4cd5d38fcc0429fa0f26591d116b1ce67
innerNULL <lyt697@outlook.com>
2021-09-06 11:55:24 +0800

    [Release] Update version.

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyfeather",
-    version="0.0.4",
+    version="0.0.5",
     author="innerNULL",
     author_email="",
     description="A project makes feature-hash easier.",
